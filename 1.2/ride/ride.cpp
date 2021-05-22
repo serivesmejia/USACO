91ea0c1ccef5f4a841361d52b186a0f268db6d84
serivesmejia <serivesmejia@gmail.com>
2021-05-22 13:13:28 -0600

    Initial commit

---
+++ 1.2/ride/ride.cpp
@@ -0,0 +1,22 @@
+/*
+ID: s3rives1
+TASK: ride
+LANG: C++
+*/
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+string name = "ride";
+
+ofstream fout (name + ".out");
+ifstream fin (name + ".in");
+
+int main()
+{
+
+  return 0;
+}
